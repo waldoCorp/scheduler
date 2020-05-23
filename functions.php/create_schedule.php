fc1692575f450242aa98fb5396d73a26e5d36f12
Ben Cerjan <bcerjan@gmail.com>
2020-05-23 17:31:57 -0500

    Recursion still not working, but moving everything to GitHub in case of a fire or something

--- functions.php/create_schedule.php
+++ functions.php/create_schedule.php
@@ -1,5 +1,5 @@
 <?php
-s
+
 /**
  * Function to create a schedule for a given week and store it in the database.
  * Will not create a new schedule if a given schedule already exists for that week.
@@ -45,8 +45,9 @@ function create_schedule() {
 
 	$requests = $stmt->fetchAll();
 
+
 	// Place some limits on our shifts:
-	$max_length = 10000000; // No max length yet
+	$max_length = 100000000; // No max length yet
 	$gap = 0.5; // Hours, mandatory gap between shifts in the same room
 							// Also the amount of time you can be working with hazards without a buddy
 	$gap = $gap *60*60; // Convert to sec from hours
@@ -66,7 +67,7 @@ function create_schedule() {
 	);
 
 	// Preference definitions: AM -> before 3 PM, PM -> after 3 PM
-	$am_cutoff = 15*60*60; // 15:00 in ms
+	$am_cutoff = 15*60*60; // 15:00 in sec
 
 	// Store all this in an object so we can easily pass it around:
 	$params = new Schedule_Parameters($start_time, $end_time, $max_length, $gap, $gap,
@@ -78,14 +79,28 @@ function create_schedule() {
 	// Start with empty schedule
 	$schedule = new Schedule;
 
-	// TESTING
-	/*foreach ($requests as $request) {
-		$schedule = add_shift($schedule, $request, $params);
-	}*/
+
 	// Now, we can recurse through all the requests and try to find the best combination:
-	$best_sched = recursive_search($requests, $schedule, $params);
-	//$best_sched = $schedule;
-	return $best_sched;
+	$best_sched = recursive_search_2($requests, $schedule, $params);
+
+	var_dump($best_sched);
+	// Finally, store the schedule in the database:
+	$week = date('Y-m-d', strtotime('Last Sunday'));
+	foreach ($best_sched->day_shifts as $day_shifts) {
+		foreach ($day_shifts as $shift) {
+			add_shift_to_db($db, $shift, $week);
+		}
+	}
+
+	echo "Number of requests: " . count($requests) . "\n";
+	echo "Number of Shifts Scheduled: " . $best_sched->count_shifts() . "\n";
+	echo "Number of Shifts Out of Pref.: " . $best_sched->get_out_num() . "\n";
+	// check if shifts overlap:
+	//echo shifts_overlap_padded($best_sched->day_shifts[0][0], $best_sched->day_shifts[0][1]) . "\n";
+	//echo shifts_overlap_padded($best_sched->day_shifts[0][1], $best_sched->day_shifts[0][0]) . "\n";
+
+	// Return true if we created a new schedule
+	return true;
 
 }
 
@@ -97,17 +112,25 @@ class Shift {
 	public $end;
 	public $room;
 	public $hazardous;
-	public $value;
+	public $out_of_pref = 0;
+	public $out_of_norm_hours = 0;
+	public $hazard_num = 0;
 	public $padded_end;
 
-	function __construct($netid, $day, $start, $end, $room, $hazardous, $value) {
+	function __construct($netid, $day, $start, $end, $room, $hazardous) {
 		$this->netid = $netid;
 		$this->day = $day;
 	 	$this->start = $start;
 		$this->end = $end;
 		$this->room = $room;
 		$this->hazardous = $hazardous;
-		$this->value = $value;
+		if ($this->hazardous) {
+			$this->hazard_num = 1;
+		}
+	}
+
+	function get_out_num() {
+		return $this->out_of_pref + $this->out_of_norm_hours;
 	}
 
 	function set_padded_end($gap) {
@@ -126,13 +149,31 @@ class Schedule {
 		5 => array(),
 		6 => array()
 	);
-	public $value = 0;
+	public $out_of_pref = 0;
+	public $out_of_norm_hours = 0;
+	public $hazard_gaps = 0;
 
 	/* Takes a Shift object and adds it to the schedule, also increments total
 	   schedule value to include new shift */
 	function add_shift_to_schedule($shift) {
 		$this->day_shifts[$shift->day][] = $shift;
-		$this->value += $shift->value;
+		$this->out_of_pref += $shift->out_of_pref;
+		$this->out_of_norm_hours += $shift->out_of_norm_hours;
+		$this->hazard_gaps += $shift->hazard_num;
+	}
+
+	function get_out_num() {
+		return $this->out_of_pref + $this->out_of_norm_hours;
+	}
+
+	function count_shifts() {
+		$i = 0;
+		foreach ($this->day_shifts as $day) {
+			foreach ($day as $shift) {
+				$i++;
+			}
+		}
+		return $i;
 	}
 
 }
@@ -161,12 +202,47 @@ class Schedule_Parameters {
 	}
 }
 
+/* Function to convert a request into a shift starting at a given time on a
+ * given day. Verifies that this is an allowed shift (respects rules)
+ *
+ * returns a Shift object if allowed or null otherwise
+ */
+function request_to_shift($current_schedule, $request, $start_time, $day, $params) {
+
+	$shift_allowed = false;
+
+	$netid = $request['netid'];
+	$room = $request['room_id'];
+	$haz = $request['hazardous'];
+	$pref = $request['time_pref'];
+
+ 	$duration = parse_duration($request['duration']);
+	if ($duration > $params->max_length) {
+		return null; // This shift is too long
+	}
+
+	$end_time = $start_time + $duration;
+	$shift = new Shift($netid, $day, $start_time, $end_time, $room, $haz);
+	$allowed = shift_allowed($current_schedule, $shift, $params);
+	echo "shift allowed to start at $start_time? $allowed \n";
+	if (shift_allowed($current_schedule, $shift, $params)) {
+		$shift->value = determine_value($shift, $request['time_pref'], $params);
+		$shift->set_padded_end($params->minimum_gap);
+		determine_hazard_status($current_schedule, $shift, $params);
+		return $shift;
+	} else {
+		return null;
+	}
+
+}
+
+
 /* Function to convert a request into a shift and returns the resulting "value"
  * associated with the new schedule.
  *
  * returns schedule with shift added (or not if it can't be)
  */
-function add_shift($current_schedule, $request, $params) {
+function add_shift($current_schedule, $shift, $params) {
 	/* Pseudocode:
 	1. Turn request into a shift by assigning it a start and end time that
 		 correspond to the duration (and a day of the week -- start with Monday)
@@ -235,42 +311,47 @@ function add_shift($current_schedule, $request, $params) {
 /* Function to determine the value of a given shift depending on the
  * preferences of the requester. Also increases value for "normal" working hours
  */
-function determine_value($shift, $pref, $params) {
-	$value = $params->pref_values['none'];
+function determine_value(&$shift, $pref, $params) {
 	$shift_start = $shift->start;
 	$shift_end = $shift->end;
 	$day = $shift->day;
-	if ($shift_end < $params->am_cutoff) { // this is an AM shift
+	$temp_val = 0;
+	if ($shift_end <= $params->am_cutoff) { // this is an AM shift
 		switch ($pref) {
 			case 'AM':
-				$value = $params->pref_values['pref'];
+				$temp_val = 0;
 				break;
 			case 'PM':
-				$value = $params->pref_values['anti'];
+				$temp_val = 2;
 				break;
 			}
-		} elseif ($shift_start > $params->am_cutoff) { // this is a PM shift
+		} elseif ($shift_start >= $params->am_cutoff) { // this is a PM shift
 			switch ($pref) {
 				case 'AM':
-					$value = $params->pref_values['anti'];
+					$temp_val = 2;
 					break;
 				case 'PM':
-					$value = $params->pref_values['pref'];
+					$temp_val = 0;
 					break;
 			}
+		} else {
+			$temp_val = 1; // If an in-between shift, it's neither best (0) nor worst (2)
 		}
 
+
 	// Now prefer ordinary working hours (09:00 - 18:00):
-	if ($shift_start <= $params->normal_start && $shift_end <= $params->normal_end) {
-		$value += 0.01;
+/*	if ( $shift_start <= $params->normal_start || $shift_end >= $params->normal_end ) {
+		$temp_val += 1;
 	}
 
 	// And prefer during the work week (Mon-Fri):
-	if (0 < $day && $day < 6){
-		$value += 0.02; // This is more valuable than being during normal hours.
+	if (0 == $day || $day == 6) {
+		$temp_val += 1; // This is more valuable than being during normal hours.
 	}
-
-	return $value;
+*/
+	$shift->out_of_norm_hours = $temp_val;
+	//echo "temp_val = " . $temp_val . "\n";
+	return;
 }
 
 /* Function to check if new shift is allowed to be added.
@@ -343,7 +424,36 @@ function find_netid_shifts($netid, $shift_array) {
 	return $out;
 }
 
+/* Function to find shifts from a schedule that at least partially overlap with
+ * a given input shift for use as a hazard buddy
+ *
+ * returns an array of shifts (or empty if none) sorted by start time
+ */
+function find_buddy_shifts($schedule, $shift) {
 
+	$day = $shift->day;
+	$day_schedule = $schedule->day_shifts[$day];
+	if (empty($schedule->day_shifts[$day])) {
+		return [];
+	}
+	$overlapping_shifts = [];
+	foreach ($day_schedule as $day_shift) {
+		if (shifts_overlap($shift, $day_shift)) {
+			// Note that this already exlcudes shifts in the same room as $shift as they can't overlap anyway
+			$overlapping_shifts[] = $day_shift;
+		}
+	}
+	usort($overlapping_shifts, "buddy_cmp");
+	return $overlapping_shifts;
+}
+
+/* Function to sort buddy shifts by start time */
+function buddy_cmp($a, $b) {
+	if ($a->start == $b->start) {
+		return 0;
+	}
+	return ($a->start < $b->start) ? -1 : 1;
+}
 /* Function to determine if two shifts overlap in time (not counting padded
  * endtime after a user leaves)
  *
@@ -382,29 +492,250 @@ function parse_duration($time_str) {
 
 }
 
-/* Function to run our recursion to search for best solution: */
-function recursive_search($requests, $schedule, $params) {
+
+/* Function to track if a shift a) needs a buddy for safety and b) if it has
+ * a buddy(ies) (or only smaller gaps)
+ */
+
+function determine_hazard_status($current_schedule, &$shift, $params) {
+	if (!$shift->hazardous) {
+		return; // This shift doesn't need a buddy
+	} else {
+		$buddy_shifts = find_buddy_shifts($current_schedule, $shift);
+		determine_overlap_interval($buddy_shifts, $shift, $params);
+	}
+}
+
+/* Recursive function to determine if a shift is totally covered (or not) by
+ * the shifts it overlaps with for safety
+ *
+ * Assumes input $buddy_shifts are sorted with the earliest ones first
+ *
+ * Updates the shift in place if it is covered and safe
+ */
+function determine_overlap_interval($buddy_shifts, &$shift, $params) {
+
+	if (empty($buddy_shifts) || $shift->hazard_num == 0) {
+		return;
+	} else {
+		$gap = $params->hazardous_gap;
+		$b_start = $buddy_shifts[0]->start;
+		$b_end = $buddy_shifts[0]->end;
+		$uncovered_shift = clone $shift;
+		if ($b_start - $gap <= $shift->start) {
+			$uncovered_shift->start = $b_end;
+			unset($buddy_shifts[0]); // Remove this shift from our buddy array
+			$buddy_shifts = array_values($buddy_shifts); // Reindex so we can reference from 0 again
+			//exit();
+			determine_overlap_interval($buddy_shifts, $uncovered_shift, $params);
+		} elseif ($b_end + $gap >= $shift->end) {
+			$uncovered_shift->end = $b_start;
+			unset($buddy_shifts[0]); // Remove this shift from our buddy array
+			determine_overlap_interval($buddy_shifts, $uncovered_shift, $params);
+		}
+	}
+
+	$duration = $uncovered_shift->end - $uncovered_shift->start;
+
+	/*if (!empty($buddy_shifts)) {
+		var_dump($buddy_shifts);
+		//exit();
+	}*/
+	if ($duration <= $gap) {
+		$shift->hazard_num = 0;
+	}
+	return;
+}
+
+// Global to store best out-of-preference number:
+$best_out = 99999;
+
+// And number of hazard shifts:
+$best_hazard = 99999;
+
+/* Function to run our recursion to search for best solution:
+ * This is the "outer loop" of the recursion, it runs only when there is a
+ * complete schedule from the sub-recursion function.
+ */
+function recursive_search_main($requests, $schedule, $params) {
 	if (empty($requests)) {
 		return $schedule;
 	} else {
+		global $best_out, $best_hazard;
+
 		$best_schedule = $schedule;
-		$best_val = $schedule->value;
+
+		// Constant time variables for when shifts can be
+		$start_time = $params->start_time;
+		$end_time = $params->end_time;
+		$increment = $params->minimum_gap; // Hours, how much do we slide a shift by before retrying?
+
 		$requests_remaining = $requests;
-		var_dump($requests);
 
 		foreach ($requests as $key => $request) {
 		  unset($requests_remaining[$key]); // Removes the element we're working on
 
-		  // try to add this request:
-		  $new_schedule = add_shift($schedule, $request, $params);
-		  if (!is_null($new_schedule)) {
-			  $new_schedule = recursive_search($requests_remaining, $new_schedule, $params);
-			  if ($new_schedule->value > $best_val) {
-				 $best_schedule = $new_schedule;
-				 $best_val = $schedule->value;
-			  }
-		  }
+			$duration = parse_duration($request['duration']);
+
+  		for ($day = 0; $day < 7; $day++) {
+				for ($t = $start_time; $t <= $end_time - $duration; $t += $increment) {
+					$this_schedule = clone $schedule;
+					$shift = request_to_shift($schedule, $request, $t, $day, $params);
+					if (!is_null($shift)) {
+						if (empty($requests_remaining && $day == 6)) {
+							echo "Shift netid: ".$shift->netid. "\n";
+						}
+						$total = $shift->get_out_num() + $this_schedule->get_out_num();
+						if ($total >= $best_out) {
+							/*echo "Score too high!!!\n";
+							var_dump($shift);
+							var_dump($this_schedule);
+							return $schedule;*/
+						} else {
+							$this_schedule->add_shift_to_schedule($shift);
+							$new_schedule = recursive_search_sub($requests_remaining, $this_schedule, $params);
+							$best_out = $new_schedule->get_out_num();
+							$best_schedule = $new_schedule;
+							$best_hazard = $new_schedule->hazard_gaps;
+							//if (empty($requests_remaining)) {
+								echo "Inside new_schedule block...\n";
+								//var_dump($best_schedule);
+							//}
+							//if (empty($requests_remaining)) {
+								echo "Number of Shifts Scheduled: " . $new_schedule->count_shifts() . "\n";
+								echo "Number of Shifts Out of Pref.: " . $new_schedule->get_out_num() . "\n";
+								//var_dump($new_schedule);
+								echo "------------------\n";
+							//}
+						}
+					}
+				}
+			}
+		}
+		echo "Number of best_Shifts Scheduled: " . $best_schedule->count_shifts() . "\n";
+		echo "Number of best_Shifts Out of Pref.: " . $best_schedule->get_out_num() . "\n";
+		return $best_schedule;
+	}
+}
+
+/* Recursive sub-function. Is the same as the _main version, but it doesn't
+ * update the hazard number or best out of pref schedule. It does check against
+ * them however.
+ */
+function recursive_search_sub($requests, $schedule, $params) {
+	if (empty($requests)) {
+		return $schedule;
+	} else {
+		global $best_out, $best_hazard;
+
+		$best_schedule = $schedule;
+
+		// Constant time variables for when shifts can be
+		$start_time = $params->start_time;
+		$end_time = $params->end_time;
+		$increment = $params->minimum_gap; // Hours, how much do we slide a shift by before retrying?
+
+		foreach ($requests as $key => $request) {
+			$requests_remaining = $requests;
+		  unset($requests_remaining[$key]); // Removes the element we're working on
+			echo "Working on request: ".$request['netid']."\n";
+			$duration = parse_duration($request['duration']);
+
+  		//for ($day = 0; $day < 7; $day++) {
+			$t = 0;
+			$day = 0;
+				for ($t = $start_time; $t <= $end_time - $duration; $t += $increment) {
+					$this_schedule = clone $schedule;
+					$shift = request_to_shift($this_schedule, $request, $t, $day, $params);
+					if (!is_null($shift)) { // This is a valid shift
+						$total = $shift->get_out_num() + $this_schedule->get_out_num();
+						if ($total >= $best_out) {
+							/*echo "Score too high!!!\n";
+							var_dump($shift);
+							var_dump($this_schedule);
+							return $schedule;*/
+						} else {
+							echo "shift_out_num: ".$shift->get_out_num()."\n";
+							echo "this_schedule_out_num: ".$this_schedule->get_out_num()."\n";
+							$this_schedule->add_shift_to_schedule($shift);
+							$new_schedule = recursive_search_sub($requests_remaining, $this_schedule, $params);
+							$best_schedule = $new_schedule;
+							$best_out = $best_schedule->get_out_num();
+							echo "Number of best_Shifts Scheduled: " . $best_schedule->count_shifts() . "\n";
+							echo "Number of best_Shifts Out of Pref.: " . $best_schedule->get_out_num() . "\n";
+							echo "best_out: $best_out\n";
+							echo "total: $total\n";
+
+							//if (empty($requests_remaining)) {
+								echo "Last scheduled netid: ".$shift->netid."\n";
+								//var_dump($best_schedule);
+							//}
+							echo "________________________\n";
+						}
+					}
+				}
+			//}
 		}
 		return $best_schedule;
 	}
+}
+
+function recursive_search_2($requests, $schedule, $params) {
+	if (empty($requests)) {
+		return $schedule;
+	} else {
+		$best_schedule = $schedule;
+		global $best_out, $best_hazard;
+
+		// Constant time variables for when shifts can be
+		$start_time = $params->start_time;
+		$end_time = $params->end_time;
+		$increment = $params->minimum_gap;
+
+		foreach($requests as $key => $request) {
+			$requests_remaining = $requests;
+			unset($requests_remaining[$key]); // Remove this request from the list
+			$this_schedule = clone $schedule;
+			$duration = parse_duration($request['duration']);
+
+			for ($day = 0; $day < 7; $day++) {
+				for ($time = $start_time; $time + $duration <= $end_time; $time += $increment) {
+					$shift = request_to_shift($this_schedule, $request, $time, $day, $params);
+					if (!is_null($shift)) {
+						if ($shift->get_out_num() + $this_schedule->get_out_num() < $best_out) {
+							$this_schedule->add_shift_to_schedule($shift);
+							$best_schedule = recursive_search_2($requests_remaining, $this_schedule, $params);
+							$best_out = $best_schedule->get_out_num();
+						}
+					}
+				}
+			}
+		}
+		return $best_schedule;
+	}
+}
+
+/* Function to add a shift to the schedule database:
+ *
+ * $week is the date of the Sunday for the week we are scheduling for.
+ */
+function add_shift_to_db($db, $shift, $week) {
+	require __DIR__ . "/table_variables.php";
+	$start_time = gmdate('H:i:s', $shift->start);
+	$end_time = gmdate('H:i:s', $shift->end);
+	$day = $week. ' + ' . $shift->day . ' days';
+	$date = date('Y-m-d', strtotime($day));
+	$start_ts = $date . " " . $start_time; // Starting timestamp
+	$end_ts = $date . " " . $end_time; // Ending timestamp
+
+	$sql = "INSERT INTO $schedule_table (netid, room_id, start_time, end_time,
+					week_start) VALUES (:netid, :room, :start_time, :end_time, :week)";
+	$stmt = $db->prepare($sql);
+	$stmt->bindValue(':netid', $shift->netid);
+	$stmt->bindValue(':room', $shift->room);
+	$stmt->bindValue(':start_time', $start_ts);
+	$stmt->bindValue(':end_time', $end_ts);
+	$stmt->bindValue(':week', $week);
+	$success = $stmt->execute();
+	return $success;
 }
