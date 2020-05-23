00ace627aeab26ab32b456c849af3d4d26987fae
Ben Cerjan <bcerjan@gmail.com>
2020-05-23 17:32:10 -0500

    Updated to make nicer calendar view

--- website/index.php
+++ website/index.php
@@ -10,6 +10,29 @@
 
 <?php include("./resources.php"); ?>
 
+<!-- Toast UI CDN -->
+<link rel="stylesheet" type="text/css" href="https://uicdn.toast.com/tui-calendar/latest/tui-calendar.css" />
+
+<!-- If you use the default popups, use this. -->
+<link rel="stylesheet" type="text/css" href="https://uicdn.toast.com/tui.date-picker/latest/tui-date-picker.css" />
+<link rel="stylesheet" type="text/css" href="https://uicdn.toast.com/tui.time-picker/latest/tui-time-picker.css" />
+
+<script src="https://uicdn.toast.com/tui.code-snippet/latest/tui-code-snippet.js"></script>
+<script src="https://uicdn.toast.com/tui.dom/v3.0.0/tui-dom.js"></script>
+<script src="https://uicdn.toast.com/tui.time-picker/latest/tui-time-picker.min.js"></script>
+<script src="https://uicdn.toast.com/tui.date-picker/latest/tui-date-picker.min.js"></script>
+<script src="https://uicdn.toast.com/tui-calendar/latest/tui-calendar.js"></script>
+<!-- End Toast UI -->
+
+<!-- Moment.JS for time/date math... -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
+
+<!-- DataTables for formatting users / requests -->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
+
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
+
+
 <style>
 .tooltip.show {
   opacity: 1 !important;
@@ -32,7 +55,7 @@
 
 require_once $function_path . 'get_names.php';
 require_once $function_path . 'get_requests.php';
-//require_once $function_path . 'get_schedule.php';
+require_once $function_path . 'get_schedule.php';
 require_once $function_path . 'get_rooms.php';
 
 
@@ -47,13 +70,13 @@ $rooms = get_rooms('DBH');
 
 // Find the schedule for the current week:
 $prior_week = date('Y-m-d', strtotime('last sunday'));
-//$schedule = get_schedule($prior_week);
+$schedule = get_schedule($prior_week);
 
 
 // Date for start of next week:
 $next_week = date('Y-m-d', strtotime('next sunday'));
 
-$schedule = [];
+//$schedule = [];
 
 
 ?>
@@ -100,7 +123,7 @@ $schedule = [];
       </form>
       <br>
       <h4>Current Users</h4>
-      <table class="table">
+      <table id="usersTable" class="table">
         <thead>
           <tr>
             <th scope="col">NetID</th>
@@ -167,13 +190,14 @@ $schedule = [];
       <br>
       <br>
       <h4>Requests for the week of <?php echo htmlspecialchars($next_week) ?></h4>
-      <table class="table">
+      <table id="requestsTable" class="table">
         <thead>
           <tr>
             <th scope="col">NetID</th>
             <th scope="col">Room Requested</th>
             <th scope="col">Duration Requested</th>
             <th scope="col">Hazardous Materials?</th>
+            <th scope="col">Remove Request</th>
           </tr>
         </thead>
         <tbody>
@@ -202,24 +226,8 @@ $schedule = [];
     <div class="collapse show" id="scheduleDiv">
     <br>
     <h4>Schedule for the week of <?php echo htmlspecialchars($prior_week) ?></h4>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">NetID</th>
-            <th scope="col">Room</th>
-            <th scope="col">Time Slot</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach($schedule as $schedule) { ?>
-            <tr>
-              <td><?php echo htmlspecialchars($request['netid']) ?></td>
-              <td><?php echo htmlspecialchars($request['room_id']) ?></td>
-              <td><?php echo htmlspecialchars($request['time_slot']) ?></td>
-            </tr>
-          <?php } ?>
-        </tbody>
-      </table>
+    <div id="calendar" style="height: 700px; width: 900px"></div>
+
     </div>
   </div>
 
@@ -229,7 +237,7 @@ $schedule = [];
 </main>
 
 <script type="text/javascript">
-
+// Removing time requests:
 $('.deleteRequest').click(function() {
   var row = $(this).closest("tr");
   var netid = row.find(".netid").html();
@@ -255,6 +263,105 @@ function deleteRequest(netid,room,duration,hazardous) {
     data: data,
   });
 }
+
+// Turn our tables into DataTables
+$(document).ready( function() {
+  $('#usersTable').DataTable();
+  $('#requestsTable').DataTable();
+})
+
+<?php // List of colors to use for calendars:
+  $txt_colors = ['#00000','#00000','#00000','#ffffff','#ffffff'];
+  $bg_colors = ['#feebe2', '#fbb4b9', '#f768a1', '#c51b8a', '#7a0177', '#00a9ff'];
+  $i = 0;
+?>
+
+
+function getPadStart(value) {
+  value = value.toString();
+
+  return value.padStart(2, '0');
+}
+
+const template = {
+  timegridDisplayPrimaryTime: function(time) {
+            var meridiem = 'am';
+            var hour = time.hour;
+
+            if (time.hour > 12) {
+                meridiem = 'pm';
+                hour = time.hour - 12;
+            }
+            return time.hour;
+            //return hour + ' ' + meridiem;
+        },
+        timegridDisplayTime: function(time) {
+            return getPadStart(time.hour) + ':' + getPadStart(time.hour);
+        },
+        timegridCurrentTime: function(timezone) {
+          var templates = [];
+
+          if (timezone.dateDifference) {
+              templates.push('[' + timezone.dateDifferenceSign + timezone.dateDifference + ']<br>');
+          }
+
+          templates.push(moment(timezone.hourmarker.toUTCString()).format('HH:mm'));
+          return templates.join('');
+        },
+}
+
+// Calendar instantiation and scheduling
+var Calendar = tui.Calendar;
+var calendar = new Calendar('#calendar', {
+  defaultView: 'week',
+  taskView: false,
+  useDetailPopup: true,
+  week: {
+    hourStart: "7",
+    hourEnd: "23"
+  },
+  timezones: [
+            {
+                timezoneOffset: parseInt(-300),
+                tooltip: 'US/Central'
+            }],
+  //template: template,
+  calendars: [
+    <?php foreach($rooms as $room) { ?>
+      {
+        id: "<?php echo htmlspecialchars($room['room_id']); ?>",
+        name: "<?php echo htmlspecialchars($room['room_id']); ?>",
+        color: "<?php echo htmlspecialchars($txt_colors[$i]); ?>",
+        bgColor: "<?php echo htmlspecialchars($bg_colors[$i]); ?>",
+        dragBgColor: "<?php echo htmlspecialchars($bg_colors[$i]); ?>",
+        borderColor: "<?php echo htmlspecialchars($bg_colors[$i]); ?>",
+      },
+    <?php $i++; } ?>
+  ]
+});
+
+// Create a calendar for each room:
+
+
+// Add our 'events'
+calendar.createSchedules([
+<?php
+$i = 0;
+foreach($schedule as $booking) { ?>
+  {
+    id: <?php echo json_encode($i); ?>,
+    calendarId: "<?php echo htmlspecialchars($booking['room_id']); ?>",
+    title: "<?php echo htmlspecialchars($booking['name']); ?>",
+    category: 'time',
+    dueDateClass: '',
+    start: "<?php echo htmlspecialchars($booking['start_time']); ?>",
+    end: "<?php echo htmlspecialchars($booking['end_time']); ?>",
+    attendees: ["<?php echo htmlspecialchars($booking['name'].", ".$booking['netid']); ?>"],
+    isReadOnly: true
+  },
+<?php $i++; } ?>
+]);
+
 </script>
 
 
