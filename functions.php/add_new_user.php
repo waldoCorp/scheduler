3dcb347fd9ba3af7e39920bccc44aee9f6df1ca3
Ben Cerjan <bcerjan@gmail.com>
2020-06-01 13:50:57 -0500

    Removed extra file that was being used as a tempalte. Added license.

--- functions.php/add_new_user.php
+++ functions.php/add_new_user.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ *    Copyright (c) 2020 Ben Cerjan, Lief Esbenshade
+ *
+ *    This file is part of Scheduler.
+ *
+ *    Scheduler is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published $
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Scheduler is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with Scheduler.  If not, see <https://www.gnu.org/licenses/>.
+**/
 
 /**
  * Function for creation of new users.
